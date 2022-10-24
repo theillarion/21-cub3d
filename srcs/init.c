c6f85874b2b3ac5ff3776ba6e1fa3761b9e97612
Illarion <pro100illarion@gmail.com>
2022-10-24 12:57:32 +0300

    Moved initial from main.c to init.c

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: illarion <glashli@student.21-school.ru>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 20:28:17 by illarion          #+#    #+#             */
-/*   Updated: 2022/10/24 12:19:51 by illarion         ###   ########.fr       */
+/*   Updated: 2022/10/24 12:57:08 by illarion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,9 @@ bool	ft_init(t_env *env)
 	ft_memset((void *)env, '\0', sizeof(*env));
 	env->settings.ceilling.data = -1;
 	env->settings.floor.data = -1;
+	env->settings.position.x += 0.5;
+	env->settings.position.y += 0.5;
+	env->settings.speed_move = SPEED_MOVE;
+	env->settings.speed_rotate = SPEED_ROTATE;
 	return (true);
 }
